484415ad155b45482033704adc9658b58f1883a4
Lidadaibiao <945827167@qq.com>
2021-10-12 18:02:06 +0800

    add Merge

--- mqdemon/src/main/java/letcode/Merge.java
+++ mqdemon/src/main/java/letcode/Merge.java
@@ -2,8 +2,34 @@ package letcode;
 
 /**
  * @author dadaibiaoLi
- * @Desc
+ * @Desc  88.合并两个有序数组
  * @Date 2021/10/12 17:10
  */
 public class Merge {
+    public static void main(String[] args) {
+
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n){
+        //正向双指针
+        int p1 = 0;  //指向 nums1 的指针
+        int p2 = 0;  // 指向 nums2 的指针
+        int[] sort = new int[m+n];
+        int cur;
+        while (p1<m||p2<n){
+            if (p1 == m){
+                cur = nums1[p2++];
+            }else if (p2 == n){
+                cur = nums1[p1++];
+            }else if (nums1[p1]<nums2[p2]){
+                cur = nums1[p1++];
+            }else {
+                cur = nums2[p2++];
+            }
+            sort[p1+p2-1] = cur;
+        }
+        for (int i = 0;i<m+n;i++){
+            nums1[i] = sort[i];
+        }
+    }
 }
